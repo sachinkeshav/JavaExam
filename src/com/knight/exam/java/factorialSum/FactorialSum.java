534e38be4809f2f6c37e8775a17d8afef4ec5504
Sachin Kushwaha <sachinkeshav@gmail.com>
2014-12-29 10:36:40 +0545

    Factorial sum implemented

--- src/com/knight/exam/java/factorialSum/FactorialSum.java
+++ src/com/knight/exam/java/factorialSum/FactorialSum.java
@@ -7,13 +7,28 @@ public class FactorialSum {
 
     static int[] solve10() {
         int fact10 = factorial(10);
+        int[] resultArray = new int[2];
+        int[] tempArray = new int[10];
 
-        return new int[]{};
+        for (int i = 0; i < 10; i++) {
+            tempArray[i] = factorial(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = i + 1; j < 10; j++) {
+                if (tempArray[i] * tempArray[j] == fact10) {
+                    resultArray[0] = i;
+                    resultArray[1] = j;
+                    return resultArray;
+                }
+            }
+        }
+        return resultArray;
     }
 
     private static int factorial(int n) {
-        if (n == 1)
+        if (n == 1 || n == 0)
             return 1;
-        else return (factorial(n) * factorial(n - 1));
+        else return (n * factorial(n - 1));
     }
 }
---
+++ src/com/knight/exam/java/factorialSum/FactorialSumTest.java
@@ -0,0 +1,13 @@
+package com.knight.exam.java.factorialSum;
+
+import java.util.Arrays;
+
+/**
+ * Created by skushwaha on 12/29/14.
+ */
+public class FactorialSumTest {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(FactorialSum.solve10()));
+    }
+}
