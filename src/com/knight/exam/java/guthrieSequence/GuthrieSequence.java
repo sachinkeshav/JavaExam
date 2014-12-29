2830800da4d2a9d3f46c6f7d3bf123d93975dbb6
Sachin Kushwaha <sachinkeshav@gmail.com>
2014-12-29 06:17:40 +0545

    Guthrie sequence logic updated.

--- src/com/knight/exam/java/guthrieSequence/GuthrieSequence.java
+++ src/com/knight/exam/java/guthrieSequence/GuthrieSequence.java
@@ -6,35 +6,17 @@ package com.knight.exam.java.guthrieSequence;
 public class GuthrieSequence {
 
     static int isGuthrieSequence(int[] a) {
-        int result = 0;
-        int num = a[0];
-
-        int[] b = new int[a.length * 2];
-        b[0] = num;
-        int counter = 1;
-
-        if (num > 0) {
-            do {
-                if (num % 2 == 0) {
-                    num = num / 2;
-                    b[counter] = num;
-                } else {
-                    num = (num * 3) + 1;
-                    b[counter] = num;
-                }
-
-                counter++;
-            } while (num != 1);
+        if (a[a.length - 1] != 1)
+            return 0;
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] % 2 == 0) {
+                if (a[i + 1] != (a[i] / 2))
+                    return 0;
+            } else {
+                if (a[i + 1] != ((a[i] * 3) + 1))
+                    return 0;
+            }
         }
-        int i;
-        for (i = 0; i < a.length; i++) {
-            if (a[i] == b[i]) {
-                result = 1;
-            } else return 0;
-        }
-
-        if (result == 1 && a[i - 1] == 1)
-            return 1;
-        else return 0;
+        return 1;
     }
 }
