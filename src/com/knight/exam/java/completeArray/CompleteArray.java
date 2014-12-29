2684523c7f58c1f9ed2bf6d4b9c8c70ef9b919ac
Sachin Kushwaha <sachinkeshav@gmail.com>
2014-12-29 20:22:47 +0545

    Complete array implemented

--- src/com/knight/exam/java/completeArray/CompleteArray.java
+++ src/com/knight/exam/java/completeArray/CompleteArray.java
@@ -7,50 +7,39 @@ public class CompleteArray {
 
     public static void main(String[] args) {
         System.out.println(isComplete(new int[]{2, 3, 2, 4, 11, 6, 10, 9, 8}));
+        System.out.println(isComplete(new int[]{2, 3, 2, 4, 11, 8, 10, 9, 8}));
         System.out.println(isComplete(new int[]{2, 3, 3, 6}));
         System.out.println(isComplete(new int[]{2, -3, 4, 3, 6}));
     }
 
     static int isComplete(int[] a) {
         int max = 0;
-        int count = 0;
 
         for (int anA : a) {
             if (anA < 0) {
                 return 0;
             }
 
-            if (anA % 2 == 0) {
-                count++;
-                if (anA > max)
-                    max = anA;
+            if (anA % 2 == 0 && anA > max) {
+                max = anA;
             }
         }
 
-        int[] tempArray = new int[count - 1];
-        int j = 0;
         for (int i = 1; i < max; i++) {
             if (i % 2 == 0) {
-                tempArray[j] = i;
-                j++;
-            }
-        }
+                boolean flag = false;
 
-        for (int temp : tempArray) {
-            boolean flag = false;
-
-            for (int anA : a) {
-                if (anA == temp) {
-                    flag = true;
-                    break;
+                for (int anA : a) {
+                    if (anA == i) {
+                        flag = true;
+                        break;
+                    }
                 }
+                if (!flag)
+                    return 0;
             }
-            if (!flag)
-                return 0;
         }
 
-//        if (count - 1 == max / 2) return 1;
-//        else return 0;
         return 1;
     }
 }
