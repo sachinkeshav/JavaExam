2b1f28d9b9a8a760de4218c60e1b9da0039eac00
Sachin Kushwaha <sachinkeshav@gmail.com>
2014-12-29 16:08:27 +0545

    Filter array implemented

--- src/com/knight/exam/java/filterArray/FilterArrayTest.java
+++ src/com/knight/exam/java/filterArray/FilterArrayTest.java
@@ -5,12 +5,12 @@ package com.knight.exam.java.filterArray;
  */
 public class FilterArrayTest {
 
-    public static void main(String []args){
-        System.out.println(FilterArray.isFilter(new int[] {1, 2, 3, 9, 6, 11}));
-        System.out.println(FilterArray.isFilter(new int[] {3, 4, 6, 7, 14, 16}));
-        System.out.println(FilterArray.isFilter(new int[] {1, 2, 3, 4, 10, 11, 13}));
-        System.out.println(FilterArray.isFilter(new int[] {3, 6, 5, 5, 13, 6, 13}));
-        System.out.println(FilterArray.isFilter(new int[] {9, 6, 18}));
-        System.out.println(FilterArray.isFilter(new int[] {4, 7, 13}));
+    public static void main(String[] args) {
+        System.out.println(FilterArray.isFilter(new int[]{1, 2, 3, 9, 6, 11}));
+        System.out.println(FilterArray.isFilter(new int[]{3, 4, 6, 7, 14, 16}));
+        System.out.println(FilterArray.isFilter(new int[]{1, 2, 3, 4, 10, 11, 13}));
+        System.out.println(FilterArray.isFilter(new int[]{3, 6, 5, 5, 13, 6, 13}));
+        System.out.println(FilterArray.isFilter(new int[]{9, 6, 18}));
+        System.out.println(FilterArray.isFilter(new int[]{4, 7, 13}));
     }
 }
