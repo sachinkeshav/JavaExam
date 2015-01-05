e0c65608104b34da5d264f92e22748103b62631f
Sachin Kushwaha <sachinkeshav@gmail.com>
2015-01-05 21:45:15 +0545

    Fine array

--- src/com/knight/exam/java/fineArray/FileArray.java
+++ src/com/knight/exam/java/fineArray/FileArray.java
@@ -6,25 +6,43 @@ package com.knight.exam.java.fineArray;
 public class FileArray {
 
     public static void main(String[] args) {
-
+        System.out.println(isFineArray(new int[]{4, 7, 9, 6, 5}));
+        System.out.println(isFineArray(new int[]{4, 9, 6, 33}));
+        System.out.println(isFineArray(new int[]{3, 8, 15}));
+        System.out.println(isFineArray(new int[]{53, 8, 15}));
     }
 
     static int isFineArray(int[] a) {
-        boolean prime1Flag = false;
-        boolean prime2Flag = false;
-
-        int p1 = 0;
-        int p2 = 0;
+        int twinPrime = 0;
+        boolean twinPrimeFlag = false;
 
         for (int i = 0; i < a.length; i++) {
             if (isPrime(a[i]) == 1) {
-                prime1Flag = true;
-                p1 = a[i] - 2;
-                p2 = a[i] + 2;
+                if (isPrime(a[i] - 2) == 0 && isPrime(a[i] + 2) == 0)
+                    return 0;
+                if (isPrime(a[i] - 2) == 1) {
+                    twinPrime = a[i] - 2;
+                    twinPrimeFlag = true;
+                }
+                if (isPrime(a[i] + 2) == 1) {
+                    twinPrime = a[i] + 2;
+                    twinPrimeFlag = true;
+                }
             }
 
-            if (prime1Flag)
+            if (twinPrimeFlag) {
+                boolean flag = false;
+                for (int j = 0; j < a.length; j++) {
+                    if (a[j] == twinPrime) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                    return 0;
+            }
         }
+        return 1;
     }
 
     static int isPrime(int n) {
