1bf1a6cb768119f5215ac2c21f68fa45ef393dfb
Sachin Kushwaha <sachinkeshav@gmail.com>
2015-01-09 16:49:21 +0545

    Friday changes at home

--- src/com/knight/exam/java/railroadTie/RailroadTie.java
+++ src/com/knight/exam/java/railroadTie/RailroadTie.java
@@ -22,7 +22,7 @@ public class RailroadTie {
     }
 
     static int isRailroadTie(int[] a) {
-        if (a.length <= 0 || a.length == 1 || a[0] == 0 || a[a.length - 1] == 0)
+        if (a.length <= 1 || a[0] == 0 || a[(a.length - 1)] == 0)
             return 0;
 
         for (int i = 1; i < a.length - 1; i++) {
